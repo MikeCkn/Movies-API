7d74d29aeeeaa1e12f56c5726400a88ef6ea78ff
Mike Ckn <michael88.chekroun@gmail.com>
2017-11-15 23:18:07 +0100

    Connect mongoDB + test next()

--- server/src/server.js
+++ server/src/server.js
@@ -6,8 +6,22 @@ import mongoose from 'mongoose';
 import config from './config/config';
 // import routes from './router/routes';
 
+//INITIALIZE DATABASE
+mongoose.connect(config.mongoUrl, () => {
+    console.log('Congrats ! DataBase initialized !')
+});
 
 const app = express();
+
+app.get('/', function(req, res, next) {
+    console.log('the response will be sent by the next function ...');
+    next();
+}, function(req, res) {
+    res.send('Hello !');
+});
+
+// app.use(router);
+
 app.listen(config.serverPort, () => {
     console.log(`Congrats ! Started on port ${config.serverPort} !`);
 });
